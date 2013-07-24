5bd1b247774e60cdab09d4fb02f6db5aedf25fe5
Kris <krishom@github.fake>
2013-07-24 02:42:50 -0700

    Detect if gjslint is installed

--- myproject/lint.sh
+++ myproject/lint.sh
@@ -1,2 +1,13 @@
 #/bin/bash
+cd $(dirname $0)
+if [[ -z "$(which gjslint)" ]]; then
+  echo "This script requires the Closure Linter"
+  echo "https://developers.google.com/closure/utilities/docs/linter_howto"
+  echo
+  echo "To install on Linux:"
+  echo "$ sudo apt-get install python-setuptools"
+  echo "$ cd /tmp"
+  echo "$ sudo easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz"
+  exit 1
+fi
 gjslint --strict --closurized_namespaces=myproject --jslint_error=all -x start-compiled.js -r .
